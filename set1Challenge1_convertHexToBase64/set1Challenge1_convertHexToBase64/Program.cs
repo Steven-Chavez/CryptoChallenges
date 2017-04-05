3a7aced0929c587a476473a1a06ce3e839a32ee2
Steven Chavez <schavez256@yahoo.com>
2017-04-04 22:31:56 -0600

    Seperated hex into two digit hex array
    This allows me to break down a hex value into two digits where I can
    turn each two digit value into a decimal

---
+++ set1Challenge1_convertHexToBase64/set1Challenge1_convertHexToBase64/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace set1Challenge1_convertHexToBase64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hexValue = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+            string[] twoDigitHexValueSeperation = new string[(hexValue.Length)];
+            
+            for(int i = 0; i < hexValue.Length; i++)
+            {
+                twoDigitHexValueSeperation[i] = hexValue.ElementAt(i).ToString() + hexValue.ElementAt(i + 1).ToString();
+                i++;
+            }
+        }
+    }
+}
