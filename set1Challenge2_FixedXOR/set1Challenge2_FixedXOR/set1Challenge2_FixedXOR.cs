8c19f6830292331d535929d69cbab517d15ba94b
Steven Chavez <schavez256@yahoo.com>
2017-04-06 19:56:30 -0600

    Created C# console application
    For set 1 challenge 2 : fix XOR

---
+++ set1Challenge2_FixedXOR/set1Challenge2_FixedXOR/set1Challenge2_FixedXOR.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace set1Challenge2_FixedXOR
+{
+    /// <summary>
+    /// Author: Steven Chavez
+    /// Date: 4/6/2017
+    /// File: set1Challenge2_FixedXOR.cs
+    /// 
+    /// Set 1 challenge 2 of the crypto challenges. Write a function
+    /// that takes two equal-length buffers and produces their XOR 
+    /// combination. http://cryptopals.com/sets/1/challenges/2
+    /// </summary>
+    class set1Challenge2_FixedXOR
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
