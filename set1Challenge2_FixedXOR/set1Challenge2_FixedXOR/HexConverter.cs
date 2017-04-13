7571f06211b287efe41ec9542564b6dcebe970ae
Steven Chavez <schavez256@yahoo.com>
2017-04-12 21:48:21 -0600

    Successfully XOR encoded two equal length hex strings

--- set1Challenge2_FixedXOR/set1Challenge2_FixedXOR/HexConverter.cs
+++ set1Challenge2_FixedXOR/set1Challenge2_FixedXOR/HexConverter.cs
@@ -9,49 +9,130 @@ namespace set1Challenge2_FixedXOR
     class HexConverter
     {
         private string hexdecimal;
+        private string xOrEncodingHex;
         private string base64;
         private string[] twoDigitHexValueSeperation;
         private byte[] byteConversion;
-        private int index = 0;
 
         public HexConverter(string _hexdecimal)
         {
             hexdecimal = _hexdecimal;
+            twoDigitHexValueSeperation = separateHexValueIntoPairs(hexdecimal);
+            byteConversion = hexToByteConversion(twoDigitHexValueSeperation);
         }
 
         public string getBase64()
         {
-            separateHexValuesIntoPairs();
-            hexToByteConversion();
             base64 = Convert.ToBase64String(byteConversion);
             return base64;
         }
 
-        private void separateHexValuesIntoPairs()
+        public byte[] getByteArray()
         {
-            twoDigitHexValueSeperation = new string[hexdecimal.Length / 2];
+            return byteConversion;
+        }
+
+        private string[] separateHexValueIntoPairs(string hex)
+        {
+            string[] hexValueSeperation = new string[hex.Length / 2];
             int index = 0;
 
             // seperate hex values into pairs 
-            for (int i = 0; i < hexdecimal.Length; i++)
+            for (int i = 0; i < hex.Length; i++)
             {
                 if (0 == i % 2)
                 {
-                    twoDigitHexValueSeperation[index] = hexdecimal.ElementAt(i).ToString() 
-                        + hexdecimal.ElementAt(i + 1).ToString();
+                    hexValueSeperation[index] = hex.ElementAt(i).ToString() 
+                        + hex.ElementAt(i + 1).ToString();
                     index++;
                 }
             }
+            return hexValueSeperation;
         }
 
-        private void hexToByteConversion()
+        private byte[] hexToByteConversion(string[] seperatedHex)
         {
-            byteConversion = new byte[twoDigitHexValueSeperation.Length];
+            byte[] conversion = new byte[twoDigitHexValueSeperation.Length];
 
-            for (int i = 0; i < byteConversion.Length; i++)
+            for (int i = 0; i < conversion.Length; i++)
             {
-                byteConversion[i] = Convert.ToByte(Convert.ToInt32(twoDigitHexValueSeperation[i], 16));
+                conversion[i] = Convert.ToByte(Convert.ToInt32(twoDigitHexValueSeperation[i], 16));
             }
+
+            return conversion;
+        }
+
+        
+        public string xOrEncoding(string _xOrEncodingHex)
+        {
+            if(_xOrEncodingHex.Length != hexdecimal.Length)
+            {
+                return "Both hex strings need to be equal-length";
+            }
+            else
+            {
+                // declare variable
+                xOrEncodingHex = _xOrEncodingHex;
+
+                // function value holding varibles 
+                byte[] hex1 = new byte[hexdecimal.Length];
+                byte[] hex2 = new byte[hexdecimal.Length];
+                string results;
+
+                hex1 = byteConversion;
+                twoDigitHexValueSeperation = separateHexValueIntoPairs(xOrEncodingHex);
+                hex2 = hexToByteConversion(twoDigitHexValueSeperation);
+
+                results = produceXOrCombination(hex1, hex2);
+                return results;
+            }
+        }
+
+        private string produceXOrCombination(byte[] hex1, byte[] hex2)
+        {
+            string[] byte1 = new string[hex1.Length];
+            string[] byte2 = new string[hex2.Length];
+            string[] encodedByte = new string[hex1.Length];
+            string encodedHex = "";
+
+            // get raw byte value for encoding
+            for (int i = 0; i < byte1.Length; i++)
+            {
+                byte1[i] = (Convert.ToString(hex1[i], 2));
+                byte2[i] = (Convert.ToString(hex2[i], 2));
+
+                if (byte1[i].Length < 8)
+                {
+                    byte1[i] = byte1[i].PadLeft(8, '0');
+                }
+
+                if (byte2[i].Length < 8)
+                {
+                    byte2[i] = byte2[i].PadLeft(8, '0');
+                }
+            }
+
+            // get XOR encoded byte string
+            for(int i = 0; i < byte1.Length; i++)
+            {
+                int encodedDecimal = 0;
+                encodedByte[i] = "";
+                for (int j = 0; j < 8; j++)
+                {
+                    if(byte1[i].ElementAt(j) == byte2[i].ElementAt(j))
+                    {
+                        encodedByte[i] += "0";
+                    }
+                    else
+                    {
+                        encodedByte[i] += "1";
+                    }
+                }
+                encodedDecimal = Convert.ToInt32((encodedByte[i]), 2);
+                encodedHex += encodedDecimal.ToString("X");
+            }
+
+            return encodedHex;
         }
     }
 }
