1648ad37e39747de04a178d4ed044d9d9e9c0585
Steven Chavez <schavez256@yahoo.com>
2017-04-27 19:00:35 -0600

    Added two fields to the EnglishTextScore class

--- set1Challenge3_XORCipher/set1Challenge3_XORCipher/EnglishTextScore.cs
+++ set1Challenge3_XORCipher/set1Challenge3_XORCipher/EnglishTextScore.cs
@@ -13,11 +13,24 @@ namespace set1Challenge3_XORCipher
     /// Version: 1.0
     /// 
     /// Scores a piece of English plain text. Used to 
-    /// score how successful a dycrypted message was. 
+    /// score how successful a decrypted message was. 
     /// The method used to score the English plain text 
     /// is done by character frequency.
     /// </summary>
     class EnglishTextScore
     {
+        /// <summary>
+        /// Total letters in string. Used for percentage 
+        /// purposes.
+        /// </summary>
+        int letterTotal;
+
+        /// <summary>
+        /// Contains each letter of the alphabet along 
+        /// with the percentage of the frequency that 
+        /// they are found in the English language.
+        /// </summary>
+        string[,] englishLetterFrequency = new string[26,26];
+
     }
 }
