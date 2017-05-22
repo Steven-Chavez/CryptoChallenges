664a1e4f034b607e1f806e9bad577ecb31fba5af
Steven Chavez <schavez256@yahoo.com>
2017-05-22 17:48:14 -0600

    Added frequency array and initialized it

--- set1Challenge3_XORCipher/set1Challenge3_XORCipher/EnglishTextScore.cs
+++ set1Challenge3_XORCipher/set1Challenge3_XORCipher/EnglishTextScore.cs
@@ -23,18 +23,25 @@ namespace set1Challenge3_XORCipher
         /// Total letters in string. Used for percentage 
         /// purposes.
         /// </summary>
-        int letterTotal;
+        private int letterTotal;
 
         /// <summary>
-        /// Contains each letter of the alphabet along 
-        /// with the percentage of the frequency that 
-        /// they are found in the English language.
+        /// Contains each letter of the alphabet 
         /// </summary>
-        string[] alphabet = new string[26]
+        private string[] alphabet = new string[26]
             {"A", "B", "C", "D", "E", "F", "G", "H",
              "I", "J", "K", "L", "M", "N", "O", "P",
              "Q", "R", "S", "T", "U", "V", "W", "X",
              "Y", "Z"};
 
+        /// <summary>
+        /// Contains each character frequency. In order
+        /// of the alphabet A-Z.
+        /// </summary>
+        private double[] frequency = new double[26]
+            {8.12, 1.49, 2.71, 4.32, 12.02, 2.30, 2.03,
+             5.92, 7.31, 0.10, 0.69, 3.98, 2.61, 6.95,
+             7.68, 1.82, 0.11, 6.02, 6.28, 9.10, 2.88,
+             1.11, 2.09, 0.17, 2.11, 0.07};
     }
 }
