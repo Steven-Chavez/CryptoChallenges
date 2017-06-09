1208b6fb6b654a412767f86b68379ea62baca4e0
Steven Chavez <schavez256@yahoo.com>
2017-06-09 17:12:29 -0600

    Added comment header for class

--- set1Challenge3_XORCipher/set1Challenge3_XORCipher/DiscoverSingleCharacterKey.cs
+++ set1Challenge3_XORCipher/set1Challenge3_XORCipher/DiscoverSingleCharacterKey.cs
@@ -6,6 +6,17 @@ using System.Threading.Tasks;
 
 namespace set1Challenge3_XORCipher
 {
+    /// <summary>
+    /// Author: Steven Chavez
+    /// Date: 6/9/17
+    /// File: DiscoverSingleCharacterKey.cs
+    /// Version: 1.0
+    /// 
+    /// Discovers the single character key by running 
+    /// an array of characters against an hex encoded
+    /// string. Each result is scored, displaying the 
+    /// top three most likely keys and their messages. 
+    /// </summary>
     class DiscoverSingleCharacterKey
     {
 
