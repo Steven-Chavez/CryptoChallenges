d63b98abd1ea063b4b9a0f92ca32cc69cab63a16
Steven Chavez <schavez256@yahoo.com>
2017-08-31 07:55:02 -0600

    Fixed while loop logic

--- set1Challenge3_XORCipher/set1Challenge3_XORCipher/EnglishTextScore.cs
+++ set1Challenge3_XORCipher/set1Challenge3_XORCipher/EnglishTextScore.cs
@@ -172,7 +172,8 @@ namespace set1Challenge3_XORCipher
                 {
                     if(index == 26)
                     {
-                        found = true;
+                        found = false;
+                        index = 0;
                     }
                     else if(messageSplit[i] == alphabet[index])
                     {
