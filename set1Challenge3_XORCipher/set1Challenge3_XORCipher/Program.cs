743cd9bc0d7a5eb7761b6ebb4d0d4d492d2f1cc9
Steven Chavez <schavez256@yahoo.com>
2017-09-02 16:50:51 -0600

    Add container for top 5 results

--- set1Challenge3_XORCipher/set1Challenge3_XORCipher/Program.cs
+++ set1Challenge3_XORCipher/set1Challenge3_XORCipher/Program.cs
@@ -18,8 +18,12 @@ namespace set1Challenge3_XORCipher
     {
         static void Main(string[] args)
         {
+            // Variables
             string hexEncodedStr = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+
             DiscoverSingleCharacterKey key = new DiscoverSingleCharacterKey(hexEncodedStr);
+            
+            
         }
     }
 }
